111b838771a281b263739e27b65ac78ee717193c
Arijeet Mukherjee <ari123acess@gmail.com>
2024-06-24 15:38:27 +0530

    Update _app.tsx

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useRef, useCallback } from "react";
 import dynamic from 'next/dynamic';
 import styles from './app.module.css';
-import useOnScreen from "@util/useOnScreen";
+import useOnScreen from "@/hooks/useOnScreen";
 import { isMobile, goTo } from "@util/index";
 import { useAppSelector } from "@store/store";
 import DefaultLayout from "@component/Layouts/DefaultLayout";
