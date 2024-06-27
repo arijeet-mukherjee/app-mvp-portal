b7eeae103b51c5cfee0cd352a7090fea7a28f6c6
Arijeet Mukherjee <ari123acess@gmail.com>
2024-06-27 20:39:49 +0530

    Update DefaultLayout.tsx

--- src/component/Layouts/DefaultLayout.tsx
+++ src/component/Layouts/DefaultLayout.tsx
@@ -3,6 +3,9 @@ import React, { useState, ReactNode } from "react";
 import Sidebar from "@/component/Sidebar";
 import Header from "@/component/Header";
 import styles from "./layout.module.css";
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from "react-redux";
+import { store, persistor } from "@store/store";
 export default function DefaultLayout({
   children,
 }: {
@@ -11,29 +14,34 @@ export default function DefaultLayout({
   const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <>
-      {/* <!-- ===== Page Wrapper Start ===== --> */}
-      <div className={styles.defaultLayout}>
-        {/* <!-- ===== Sidebar Start ===== --> */}
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* <!-- ===== Sidebar End ===== --> */}
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
 
-        {/* <!-- ===== Content Area Start ===== --> */}
-        <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-          {/* <!-- ===== Header Start ===== --> */}
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          {/* <!-- ===== Header End ===== --> */}
+          {/* <!-- ===== Page Wrapper Start ===== --> */}
+          <div className={styles.defaultLayout}>
+            {/* <!-- ===== Sidebar Start ===== --> */}
+            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            {/* <!-- ===== Sidebar End ===== --> */}
 
-          {/* <!-- ===== Main Content Start ===== --> */}
-          <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              {children}
+            {/* <!-- ===== Content Area Start ===== --> */}
+            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+              {/* <!-- ===== Header Start ===== --> */}
+              <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+              {/* <!-- ===== Header End ===== --> */}
+
+              {/* <!-- ===== Main Content Start ===== --> */}
+              <main>
+                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                  {children}
+                </div>
+              </main>
+              {/* <!-- ===== Main Content End ===== --> */}
             </div>
-          </main>
-          {/* <!-- ===== Main Content End ===== --> */}
-        </div>
-        {/* <!-- ===== Content Area End ===== --> */}
-      </div>
-      {/* <!-- ===== Page Wrapper End ===== --> */}
+            {/* <!-- ===== Content Area End ===== --> */}
+          </div>
+          {/* <!-- ===== Page Wrapper End ===== --> */}
+        </PersistGate>
+      </Provider>
     </>
   );
 }
